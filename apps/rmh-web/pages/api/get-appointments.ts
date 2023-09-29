0950b7c326214de539b5ecac97b95b24c2f090f9
aballerr <aball9423@gmail.com>
2023-09-29 11:14:43 -0600

    added try catch to graphql endpoint

--- apps/rmh-web/pages/api/get-appointments.ts
+++ apps/rmh-web/pages/api/get-appointments.ts
@@ -12,39 +12,43 @@ export default async function GET(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
-  const document = gql`
-query MyQuery {
-  getAppointments(ptId: "${request.query.ptdid}") {
-    items {
-      appointmentDate
-      createdOn
-      duration
-      patientName
-      ptId
-      zoomUrl
+  try {
+    const document = gql`
+    query MyQuery {
+      getAppointments(ptId: "${request.query.ptdid}") {
+        items {
+          appointmentDate
+          createdOn
+          duration
+          patientName
+          ptId
+          zoomUrl
+        }
+      }
     }
+    `;
+
+    const GRAPHQL_API_URL_API_KEY = process.env.GRAPHQL_API_URL_API_KEY
+      ? process.env.GRAPHQL_API_URL_API_KEY
+      : "";
+    const GRAPHQL_API_URL = process.env.GRAPHQL_API_URL
+      ? process.env.GRAPHQL_API_URL
+      : "";
+    console.log(
+      `API_KEY ${GRAPHQL_API_URL_API_KEY}, GRAPHQL_API_URL ${GRAPHQL_API_URL}`
+    );
+
+    const result: Result = await GQLRequest(
+      GRAPHQL_API_URL,
+      document,
+      undefined,
+      {
+        "x-api-key": GRAPHQL_API_URL_API_KEY,
+      }
+    );
+
+    response.status(200).json(result.getAppointments.items);
+  } catch (err) {
+    response.status(500).json({ err });
   }
-}
-`;
-
-  const GRAPHQL_API_URL_API_KEY = process.env.GRAPHQL_API_URL_API_KEY
-    ? process.env.GRAPHQL_API_URL_API_KEY
-    : "";
-  const GRAPHQL_API_URL = process.env.GRAPHQL_API_URL
-    ? process.env.GRAPHQL_API_URL
-    : "";
-  console.log(
-    `API_KEY ${GRAPHQL_API_URL_API_KEY}, GRAPHQL_API_URL ${GRAPHQL_API_URL}`
-  );
-
-  const result: Result = await GQLRequest(
-    GRAPHQL_API_URL,
-    document,
-    undefined,
-    {
-      "x-api-key": GRAPHQL_API_URL_API_KEY,
-    }
-  );
-
-  response.status(200).json(result.getAppointments.items);
 }
